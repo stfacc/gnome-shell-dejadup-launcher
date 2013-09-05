4604ab6cc10829e3ee8db1eb75b0c49603c4e5f2
Stefano Facchini <stefano.facchini@gmail.com>
2013-09-05 23:31:51 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+DejaDup Launcher GNOME Shell extension
+======================================
+
+When the disk used by DejaDup for the backups is plugged, a button
+is added to the run notification which allow the user to immediately
+start a backup.
---
+++ extension.js
@@ -0,0 +1,115 @@
+const Lang = imports.lang;
+
+const Gio = imports.gi.Gio;
+const St = imports.gi.St;
+
+const MessageTray = imports.ui.messageTray;
+const AutorunManager = imports.ui.components.autorunManager;
+
+const DEJADUP_SCHEMA = 'org.gnome.DejaDup.File';
+
+const ModifiedAutorunTransientNotification = new Lang.Class({
+    Name: 'ModifiedAutorunTransientNotification',
+
+    _init: function() {
+        this._dejadupInstalled = false;
+
+        let schemas = Gio.Settings.list_schemas();
+        if (schemas.indexOf(DEJADUP_SCHEMA) == -1)
+            return;
+
+        this._dejadupInstalled = true;
+
+        this._originalInit = AutorunManager.AutorunTransientNotification.prototype._init;
+
+    },
+
+    _modifiedInit: function() {
+        return function(manager, source) {
+            MessageTray.Notification.prototype._init.call(this, source, source.title, null, { customContent: true });
+
+            ////////////////////////////////////////
+            // Normal content of the notification //
+            ////////////////////////////////////////
+            this._manager = manager;
+            this._box = new St.BoxLayout({ style_class: 'hotplug-transient-box',
+                                           vertical: true });
+            this.addActor(this._box);
+
+            this._mount = source.mount;
+
+            source.apps.forEach(Lang.bind(this, function (app) {
+                let actor = this._buttonForApp(app);
+
+                if (actor)
+                    this._box.add(actor, { x_fill: true,
+                                           x_align: St.Align.START });
+            }));
+
+            this._box.add(this._buttonForEject(), { x_fill: true,
+                                                    x_align: St.Align.START });
+
+            // set the notification to transient and urgent, so that it
+            // expands out
+            this.setTransient(true);
+            this.setUrgency(MessageTray.Urgency.CRITICAL);
+
+            ///////////////////////////
+            // Our new Backup button //
+            ///////////////////////////
+            let volume = this._mount.get_volume();
+            if (!volume)
+                return;
+
+            let uuid = volume.get_identifier(Gio.VOLUME_IDENTIFIER_KIND_UUID);
+
+            let settings = new Gio.Settings({ schema: DEJADUP_SCHEMA });
+            let backupUuid = settings.get_string('uuid');
+
+            if (uuid != backupUuid)
+                return;
+
+            let box = new St.BoxLayout();
+            let icon = new St.Icon({ icon_name: 'deja-dup-symbolic',
+                                     style_class: 'hotplug-notification-item-icon' });
+            box.add(icon);
+
+            let label = new St.Bin({ y_align: St.Align.MIDDLE,
+                                     child: new St.Label
+                                     ({ text: "Backup" })
+                                   });
+            box.add(label);
+
+            let button = new St.Button({ child: box,
+                                         x_fill: true,
+                                         x_align: St.Align.START,
+                                         button_mask: St.ButtonMask.ONE,
+                                         style_class: 'hotplug-notification-item' });
+
+            button.connect('clicked', Lang.bind(this, function() {
+                let app = Gio.DesktopAppInfo.new('deja-dup.desktop');
+                app.launch([], global.create_app_launch_context());
+
+                this.destroy();
+            }));
+
+            this._box.add(button, { x_fill: true,
+                                    x_align: St.Align.START });
+        };
+    },
+
+    enable: function() {
+        if (this._dejadupInstalled)
+            AutorunManager.AutorunTransientNotification.prototype._init = this._modifiedInit();
+    },
+
+    disable: function() {
+        if (this._dejadupInstalled)
+            AutorunManager.AutorunTransientNotification.prototype._init = this._originalInit;
+    }
+});
+
+
+function init() {
+    return new ModifiedAutorunTransientNotification();
+}
